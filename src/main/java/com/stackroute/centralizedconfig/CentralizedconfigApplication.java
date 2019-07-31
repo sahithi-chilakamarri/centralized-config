667be6f4f18702f4ee3d944d84a08b7a5997406a
Sahithi <sahithimpc@gmail.com>
2019-07-31 12:43:33 +0530

    Centralized config

---
+++ src/main/java/com/stackroute/centralizedconfig/CentralizedconfigApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.centralizedconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedconfigApplication.class, args);
+	}
+
+}
